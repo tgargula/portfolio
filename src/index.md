b82b5b2754486b7b86c05e10a0200a74b7967c40
tgargula <tomaszgargula1@gmail.com>
2025-03-10 19:02:39 +0100

    chore: modify home page

--- src/index.md
+++ src/index.md
@@ -1,11 +1,10 @@
 ---
-title: Hello there!
-layout: base.njk
+title: Hello
+subtitle: I am <span class="primary">Tomek</span>. Full-stack engineer.
+layout: home.njk
 ---
 
-My name is Tomek. Thanks for visiting my site! <b>This page is currently under construction.</b>
-
-## Posts
+# Posts
 Check out some recommendations that I have written that are based on my travels!
 {% for post in collections.posts %}
 - [{{ post.data.title }}]({{ post.url }})
---
+++ src/scripts/home.js
@@ -0,0 +1,86 @@
+
+// Particles
+
+const TTL = 1000;
+
+function animateParticle(particle, physics, startTime, currentTime) {
+    if (currentTime - startTime > TTL) {
+        particle.remove();
+        return;
+    }
+
+    const position = {
+        x: physics.position.x + physics.velocity.x,
+        y: physics.position.y + physics.velocity.y,
+    }
+    const velocity = {
+        x: physics.velocity.x,
+        y: physics.velocity.y + physics.gravity,
+    }
+
+    particle.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    particle.style.opacity = 1 - (currentTime - startTime) / TTL;
+
+    requestAnimationFrame((timestamp) => animateParticle(particle, { ...physics, position, velocity }, startTime, timestamp));
+}
+
+document.addEventListener('mousemove', (event) => {
+    for (let i = 0; i < 2; i++) {
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        particle.style.left = `${event.pageX}px`;
+        particle.style.top = `${event.pageY}px`;
+
+        const angle = Math.random() * Math.PI * 2;
+        const physics = {
+            position: { x: 0, y: 0 },
+            velocity: { x: 10 * Math.random() * Math.cos(angle), y: 10 * Math.random() * Math.sin(angle) },
+            gravity: 1 / 2,
+        };
+
+        document.body.appendChild(particle);
+        requestAnimationFrame((t) => animateParticle(particle, physics, t));
+    }
+})
+
+document.addEventListener('touchmove', (event) => {
+    for (const touch of event.touches) {
+        for (let i = 0; i < 3; i++) {
+            const particle = document.createElement('div');
+            particle.classList.add('particle');
+            particle.style.left = `${touch.pageX}px`;
+            particle.style.top = `${touch.pageY}px`;
+
+            const angle = Math.random() * Math.PI * 2;
+            const physics = {
+                position: { x: 0, y: 0 },
+                velocity: { x: 10 * Math.random() * Math.cos(angle), y: 10 * Math.random() * Math.sin(angle) },
+                gravity: 1 / 2,
+            };
+
+            document.body.appendChild(particle);
+            requestAnimationFrame((t) => animateParticle(particle, physics, t));
+        }
+    }
+})
+
+document.addEventListener('click', (event) => {
+    for (let i = 0; i < 18; i++) {
+
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        particle.style.left = `${event.pageX}px`;
+        particle.style.top = `${event.pageY}px`;
+
+        const angle = i / 18 * Math.PI * 2;
+        const physics = {
+            position: { x: 0, y: 0 },
+            velocity: { x: 10 * Math.random() * Math.cos(angle), y: 10 * Math.random() * Math.sin(angle) },
+            gravity: 1 / 2,
+        };
+
+        requestAnimationFrame((t) => animateParticle(particle, physics, t));
+
+        document.body.appendChild(particle);
+    }
+})
